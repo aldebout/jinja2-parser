4e0000ad5382a64139f537e3bbb7a5d2b3b20f04
aldebout <alexdeboutray@gmail.com>
2017-12-20 10:10:37 +0100

    Pretty doc and unchecked show whitespace by default

--- filters.py
+++ filters.py
@@ -6,13 +6,21 @@ from dateutil import parser
 from base64 import b64encode
 import json
 
-def filter_leetify(a, **kw):
-    """Leetify text ('a' becomes '4', 'e' becomes '3', etc.)"""
-    return a.replace('a','4').replace('e','3').replace('i','1').replace('o','0').replace('u','^')
-
+def filter_to_date(input, tzname='Europe/Paris'):
+    """
+        Parses a string into a datetime object, takes tzname as parameter.
+        {{ "20171212T1212" |to_date }}
+    """
+    date = parser.parse(input)
+    if date.tzinfo == None :
+        return timezone(tzname).localize(date)
+    return date
 
 def filter_datetime(value, format='date', tz='Europe/Paris', locale='fr_FR'):
-    '''Pretty prints a datetime object according to parameters like format (in UTS-35 standard), tz and locale'''
+    """
+        Pretty prints a datetime object according to parameters like format (in UTS-35 standard), tz and locale.
+        {{ "20171212T1212" |to_date |datetime("EEEE dd-MM-yyyy 'à' HH'h'mm", "CET", locale='es_ES') }}
+    """
     if format == 'date':
         format="dd/MM/yyyy"
     elif format == 'timeh':
@@ -25,24 +33,39 @@ def filter_datetime(value, format='date', tz='Europe/Paris', locale='fr_FR'):
         format="dd MMMM"
     return format_datetime(value, format, tzinfo=get_timezone(tz), locale=locale)
 
-def filter_to_date(input, tzname='Europe/Paris'):
-    '''Parses a string into a datetime object, takes tzname as parameter'''
-    date = parser.parse(input)
-    if date.tzinfo == None :
-        return timezone(tzname).localize(date)
-    return date
-
 def filter_timedelta(delta, granularity='minute', threshold=2, add_direction=False , format='short', locale='fr_FR'):
-    '''Pretty prints a timedelta object (difference between two datetimes). Takes parameters like granularity (minute, hour...), threshold (elasticity to granularity), format and locale.'''
+    """
+        Pretty prints a timedelta object (difference between two datetimes).
+        Defaults: minute, 2, False , short, fr_FR
+    """
     return format_timedelta(delta, granularity=granularity, threshold=threshold, add_direction=add_direction, format=format, locale=locale)
 
 def filter_b64encode(string):
-    '''Encodes an utf-8 string to base64'''
+    """
+        Encodes an utf-8 string to base64
+        {{ "Alexandre" |b64encode |safe }}
+    """
     return b64encode(bytes(string, 'utf-8')).decode('UTF-8')
 
-filter_to_json = json.dumps
-
-filter_from_json = json.loads
-
 def filter_to_utf8(string):
+    """
+        Fixes some encoding problems that can be encountered when scraping the web.
+        {{ "Nanterre-UniversitÃ©" |to_utf8 }}
+    """
     return string.encode('LATIN-1').decode('UTF-8')
+
+def filter_to_json( *args, **kwargs ):
+    """
+        Pretty prints à python object to valid JSON. Do not forget to use |safe.
+        Wrapper for json.dumps, see official doc.
+        {{ any_dict |to_json |safe }}
+    """
+    return json.dumps( *args, **kwargs)
+
+def filter_from_json( *args, **kwargs ):
+    """
+        Loads a JSON string to a Python dict.
+        Wrapper for json.loads, see official doc.
+        {{ "{\"name\":\"Alexandre\"}" |from_json }}
+    """
+    return json.loads( *args, **kwargs)
